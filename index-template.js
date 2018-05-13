5dfbe12a45fb354e1f9b1f52af7eda568fbf15da
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-05-13 18:16:52 +0800

    Added module-template

---
+++ index-template.js
@@ -0,0 +1,22 @@
+/*
+
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+
+app.listen(PORT);
+console.log('\n------------------------------------------ Server Up ------------------------------------------\n');
+
+*/
+
+//*******************************************************************************************************************//
+
+// Using your pre-defined module
+var moduleTemplate = require("./module-template/module-template");
+var moduleTemplateObject = moduleTemplate();
+moduleTemplateObject.printHelloWorld();
+
+//*******************************************************************************************************************//
+
+
---
+++ module-template/module-template.js
@@ -0,0 +1,10 @@
+module.exports = function (){
+
+	var moduleTemplate = {};
+
+	moduleTemplate.printHelloWorld = function (){
+		console.log("Hello World");
+	}
+
+	return moduleTemplate;
+}
