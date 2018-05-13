9dee461a0bea56d3d73435af1968059efdb77d21
Thanh Nam Nguyen <nam4@e.ntu.edu.sg>
2018-05-13 18:42:45 +0800

    Added db template

--- index-template.js
+++ index-template.js
@@ -12,11 +12,21 @@ console.log('\n------------------------------------------ Server Up ------------
 
 //*******************************************************************************************************************//
 
+/*
+
 // Using your pre-defined module
 var moduleTemplate = require("./module-template/module-template");
 var moduleTemplateObject = moduleTemplate();
 moduleTemplateObject.printHelloWorld();
 
+*/
+
 //*******************************************************************************************************************//
 
+var db = require("./db/db.js");
+var dbInstance = db();
+
+dbInstance.updateData(187, "This is data Id 187");
+dbInstance.queryData(187);
+dbInstance.queryData(718);
 
